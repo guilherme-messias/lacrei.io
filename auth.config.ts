496be860f25b80356fd6418ac4edf0f6626e0bc7
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-06 15:35:05 -0300

    feat: refatora configuração de autenticação com NextAuth e adiciona arquivo de inicialização

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,23 @@
 import type { NextAuthConfig } from 'next-auth'
-import Google from 'next-auth/providers/google'
-import Spotify from 'next-auth/providers/spotify'
+import GoogleProvider from 'next-auth/providers/google'
+import SpotifyProvider from 'next-auth/providers/spotify'
 
 export const authConfig = {
-  providers: [Google, Spotify],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      allowDangerousEmailAccountLinking: true,
+    }),
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID ?? '',
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET ?? '',
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
   pages: {
     signIn: '/login',
+    signOut: '/logout',
   },
   callbacks: {
     session({ session, token }) {
